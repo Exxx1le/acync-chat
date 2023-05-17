745342158722af379e50789e174962e80d7f44ed
Ex1le <3x1le@rambler.ru>
2023-05-17 11:50:57 +0300

    lesson 1

---
+++ lesson_1/task_2.py
@@ -0,0 +1,10 @@
+# 2. Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность кодов 
+# (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
+
+a = b'class'
+b = b'function'
+c = b'method'
+
+print(a, type(a), (len(a)))
+print(b, type(b), (len(b)))
+print(c, type(c), (len(c)))
