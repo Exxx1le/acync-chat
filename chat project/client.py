6d2d7978b89c7db65cc3355af19d9765f7751480
Ex1le <3x1le@rambler.ru>
2023-06-08 22:04:49 +0300

    last message responce

--- chat project/client.py
+++ chat project/client.py
@@ -8,20 +8,24 @@ logger = client_logger.logger
 @log
 def send_message():
     with socket(AF_INET, SOCK_STREAM) as sock:
-        server_address = ("localhost", 7777)
-        sock.connect(server_address)
-        while True:
-            message = input("Введите сообщение: ")
-            try:
-                sock.send(message.encode("utf-8"))
-                data = sock.recv(4096).decode("utf-8")
-                print(data)
-                logger.debug(f"На сервер отправлено сообщение {message}")
-                logger.debug(f"Получено сообщение {data}")
-            except ConnectionRefusedError as e:
-                logger.critical(f"В соединении отказано {e}")
-            except Exception as e:
-                logger.exception("Возникло исключение при отправке сообщения {e}")
+        try:
+            server_address = ("localhost", 7777)
+            sock.connect(server_address)
+        except ConnectionRefusedError as e:
+            logger.critical(f"В соединении отказано {e}")
+        finally:
+            while True:
+                message = input("Введите сообщение: ")
+                print(f"Ваше сообщение {message}")
+                try:
+                    sock.send(message.encode("utf-8"))
+                    data = sock.recv(4096).decode("utf-8")
+                    print(data)
+                    logger.debug(f"Получены данные {data}")
+                    logger.debug(f"Получено сообщение {data}")
+
+                except Exception as e:
+                    logger.exception("Возникло исключение при отправке сообщения {e}")
 
 
 if __name__ == "__main__":
--- chat project/server.py
+++ chat project/server.py
@@ -8,6 +8,34 @@ logger = server_logger.logger
 
 
 @log
+def read_requests(read_clients, all_clients) -> list:
+    recieved_messages = []
+
+    for sock in read_clients:
+        try:
+            data = sock.recv(1024).decode("utf-8")
+            recieved_messages.append(data)
+        except Exception:
+            logger.info(f"Клиент {sock.fileno()} {sock.getpeername()} отключился")
+            all_clients.remove(sock)
+
+    return recieved_messages
+
+
+@log
+def write_responses(messages_list, write_clients, all_clients):
+    for sock in write_clients:
+        try:
+            # response = requests.get(sock)
+            message = messages_list[-1]
+            sock.send(message.encode("utf-8"))
+        except Exception:
+            logger.info(f"Клиент {sock.fileno()} {sock.getpeername()} отключился")
+            sock.close()
+            all_clients.remove(sock)
+
+
+@log
 def start_server():
     with socket(AF_INET, SOCK_STREAM) as server_cock:
         server_cock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -21,19 +49,23 @@ def start_server():
             try:
                 client_sock, address = server_cock.accept()
             except OSError as e:
+                # logger.critical(f"Ошибка сервера {e}")
                 pass
             else:
                 client_sockets.append(client_sock)
+                logger.info(f"Получен запрос на соединение от {str(address)}")
             finally:
-                for sock in client_sockets:
-                    cl_read = []
-                    cl_write = []
-                    cl_read, cl_write, _ = select(client_sockets, client_sockets, [], 0)
-                    if sock in cl_read:
-                        recieved_data = sock.recv(4096)
-                        data = recieved_data.decode("utf-8")
-                        logger.info(f"Получено сообщение {data}")
-                        sock.send(data.encode("utf-8"))
+                cl_read = []
+                cl_write = []
+                cl_read, cl_write, _ = select(client_sockets, client_sockets, [], 0)
+                # if sock in cl_read:
+                #     recieved_data = sock.recv(4096)
+                #     data = recieved_data.decode("utf-8")
+                #     logger.info(f"Получено сообщение {data}")
+                #     sock.send(data.encode("utf-8"))
+                requests = read_requests(cl_read, client_sockets)
+                if requests:
+                    write_responses(requests, cl_write, client_sockets)
 
 
 if __name__ == "__main__":
